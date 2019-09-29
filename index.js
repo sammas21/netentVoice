18ffdea2001af7f2cf3c10fcbe13f50dd05a3720
Sumit Maurya <Sumit.Maurya@netent.com>
2019-09-30 04:26:37 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var cors = require('cors');
+
+app.use(express.static("public"));
+
+app.use(cors());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+function pushData(data){
+    console.log("dis go to this");
+    io.emit('chat message', data);
+}
+
+app.get('/msg', function(req, res){
+    let data = req.query['data'];
+    pushData(data);
+    console.log(data);
+    res.send(data)
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
